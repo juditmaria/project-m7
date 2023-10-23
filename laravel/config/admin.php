6df508915350f4ff9facea8f5786e4ff3fb98a46
juditmaria <jumoar@fp.insjoaquimmir.cat>
2023-10-23 19:41:44 +0200

    Codi base del projecte

---
+++ laravel/config/admin.php
@@ -0,0 +1,8 @@
+<?php
+
+
+return [
+   'name'      => env('ADMIN_NAME', 'daw2_08'),
+   'email'     => env('ADMIN_EMAIL', '2daw.equip08@fp.insjoaquimmir.cat'),
+   'password'  => env('ADMIN_PASSWORD', 'dKt49z441![*'),
+];
