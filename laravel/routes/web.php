83ccd400b13e42ade92235417b67fb67fde860c9
juditmaria <jumoar@fp.insjoaquimmir.cat>
2023-10-24 19:27:06 +0200

    Fent verificació de correu

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
-/*use Illuminate\Support\Facades\Route;*/
-/*use Illuminate\Support\Facades\Log;*/
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,20 +14,18 @@
 |
 */
 
-/*
 Route::get('/', function () {
     return view('welcome');
 });
-*/
 
-/*
-Route::get('/', function () {
-   Log::info('Loading welcome page');
-   return view('welcome');
-});
-*/
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::get('/', function () {
-   Debugbar::info('hola');
-   return view('welcome');
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+require __DIR__.'/auth.php';
